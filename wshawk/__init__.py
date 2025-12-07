3aa07ae2f0811d11233da25d23e43cb36d40ec05
noobforanonymous <webdevelopment123.0.0.1@gmail.com>
2025-12-07 01:26:09 -0800

    Fix syntax error in __init__.py (v1.0.6)

--- wshawk/__init__.py
+++ wshawk/__init__.py
@@ -2,8 +2,8 @@
 WSHawk - WebSocket Security Scanner
 """
 
-from .__ main__ import *
+from .__main__ import *
 from .interactive import *
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __author__ = "Regaan (@noobforanonymous)"
